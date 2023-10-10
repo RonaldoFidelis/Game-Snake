609667a450bbe6fef3d98dfa86506d24c3286db6
Ronaldo <ronaldofidelis.ti@gmail.com>
2023-10-10 17:56:15 -0300

    Feat: DrawnSnakeAndMoveSnake

--- src/main.js
+++ src/main.js
@@ -4,11 +4,16 @@ class Game {
   constructor(){
     this.ctx = canvas.getContext("2d"),
     this.size = 30,
-    this.snake = [{x:200, y:200},{x:230, y:200}]
+    this.snake = [{x:200, y:200},{x:230, y:200}],
+    this.direction = ''
   }
 
   initGame(){
-
+    setInterval(()=> {
+      this.ctx.clearRect(0,0, 500,500)
+      this.moveSnake()
+      this.drawSnake()
+    }, 300)
   }
 
   drawSnake() {
@@ -22,11 +27,27 @@ class Game {
   }
 
   moveSnake(){
-    
+    const head = this.snake[this.snake.length - 1];
+    if(!this.direction) return
+
+    if(this.direction == 'right'){
+      this.snake.push({x: head.x + this.size, y: head.y})
+    }
+    if(this.direction == 'left'){
+      this.snake.push({x: head.x - this.size, y: head.y})
+    }
+    if(this.direction == 'up'){
+      this.snake.push({x: head.x, y: head.y - this.size})
+    }
+    if(this.direction == 'down'){
+      this.snake.push({x: head.x, y: head.y + this.size})
+    }
+
+    this.snake.shift()
   }
 
 }
 
 const game = new Game();
 
-game.drawSnake()
+game.initGame()
