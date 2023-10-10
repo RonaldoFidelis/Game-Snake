9a84a6edec0f10e5d9b5cf4388689ca30219078c
Ronaldo <ronaldofidelis.ti@gmail.com>
2023-10-10 20:20:39 -0300

    Feat: CreateAppleAndInitCheckEat

--- src/main.js
+++ src/main.js
@@ -3,11 +3,21 @@ document.addEventListener("keydown", (e) => {
   console.log(e.key)
 })
 
+const number = (min, max) => {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
 class Game {
   constructor() {
     this.ctx = canvas.getContext("2d");
     this.size = 20;
-    this.snake = [{ x: 200, y: 200 }, { x: 220, y: 200 }];
+    this.snake = [{ x: 200, y: 200}];
+    this.colorApple = [{name: "vermelho", color:"#D70419"},{name:"dourado", color:"#EDCB23"},{name: "verde", color:"#68D60D"}];
+    this.apple = {
+      x:this.randomPositionAplle(),
+      y:this.randomPositionAplle(),
+      tipo: this.randomColorApple(),
+    };
     this.pontuation = 0;
     this.direction = '';
     this.difficulty = 'medium'
@@ -29,17 +39,37 @@ class Game {
   }
 
   renderGame(){
-    this.getDirection()
     this.ctx.clearRect(0, 0, 500, 500);
+    this.drawApple();
+    this.eatApple();
+    this.getDirection();
     this.moveSnake();
     this.drawSnake();
   }
 
+  randomPositionAplle(){
+    let num = number(0, canvas.width - this.size);
+    return Math.round(num / 20) * 20;
+  }
+
+  randomColorApple(){
+    let num = number(0,2);
+    let {name, color} = this.colorApple[num];
+    return {name,color};
+  }
+
+  drawApple(){
+    const {x, y, tipo} = this.apple;
+
+    this.ctx.fillStyle = tipo.color;
+    this.ctx.fillRect(x, y, this.size, this.size);
+  }
+
   drawSnake() {
-    this.ctx.fillStyle = "white"
+    this.ctx.fillStyle = "#0AB19F"
     this.snake.forEach((position, index) => {
       if (index == this.snake.length - 1) {
-        this.ctx.fillStyle = "green"
+        this.ctx.fillStyle = "#08C28D"
       }
       this.ctx.fillRect(position.x, position.y, this.size, this.size)
     })
@@ -67,19 +97,29 @@ class Game {
 
   getDirection(){
     document.addEventListener("keydown", ({key})=>{
-      if(key == "ArrowRight"){
+      if(key == "ArrowRight" && this.direction != 'left'){
         this.direction = 'right'
       }
-      if(key == "ArrowLeft"){
+      if(key == "ArrowLeft" && this.direction != 'right'){
         this.direction = 'left'
       }
-      if(key == "ArrowUp"){
+      if(key == "ArrowUp" && this.direction != 'down'){
         this.direction = 'up'
       }
-      if(key == "ArrowDown"){
+      if(key == "ArrowDown" && this.direction != 'up'){
         this.direction = 'down'
       }
     })
+  }
+
+  eatApple() {
+    const head = this.snake[this.snake.length-1];
+    const {x, y} = head;
+
+    if(x == this.apple.x && y == this.apple.y){
+      console.log('comeu');
+    }
+
   }
 }
 
